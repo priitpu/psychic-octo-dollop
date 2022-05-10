be7463600c091d35230a2653dae6c916885dd16d
Priit Puru <priit.puru@twn.ee>
2022-05-10 15:46:14 +0300

    .

--- js.js
+++ js.js
@@ -1,3 +1,5 @@
+var tags = [];
+
 const getMetadata = async () => {
   const req = await fetch("./library/mtime.json");
   const res = await req.json();
@@ -5,14 +7,17 @@ const getMetadata = async () => {
   const promises = folders.map(async (folder) => {
     return await getFiles(folder);
   });
-  const allPromises = await Promise.allSettled(promises);
-  const images = allPromises
-    .filter((thing) => thing.status === "fulfilled")
-    .map((thing) => thing.value);
-  images.forEach((image) => generateImageHTML(image));
+  const allPromises = await Promise.allSettled([...promises]);
+  const images = allPromises.map((thing) => thing.value);
+  images.forEach((img) => {
+    if (img) {
+      generateImageHTML(img);
+    }
+  });
+  tags.forEach((tag) => generateTagsHtml1(tag));
 };
 
-const getFiles = async (folder) => {
+var getFiles = async (folder) => {
   try {
     const req = await fetch(`./library/images/${folder}.info/metadata.json`);
     const metadata = await req.json();
@@ -24,26 +29,35 @@ const generateImageHTML = (image) => {
   const temp = document.querySelector(".image-template");
   const clone = temp.content.cloneNode(true);
   clone.querySelector(".image-template__wrapper").dataset.id = image.folder;
-  clone.querySelector(".image-template__tags").innerHTML =
-    image.metadata.tags.join(", ");
+  image.metadata.tags.forEach((tag) => {
+    clone.querySelector(".image-template__wrapper").dataset[tag] = true;
+  });
+  tags = new Set([...tags, ...image.metadata.tags]);
   clone.querySelector(
     ".image-template__img"
-  ).src = `./library/images/${image.folder}.info)/${image.metadata.name}.${image.metadata.ext}`;
+  ).src = `./library/images/${image.folder}.info/${image.metadata.name}.${image.metadata.ext}`;
   document.querySelector(".images").appendChild(clone);
-  generateTagsHtml(image.folder);
 };
 
-const generateTagsHtml = (folder) => {
+var generateTagsHtml1 = (tag) => {
   const temp = document.querySelector(".tags-template");
   const clone = temp.content.cloneNode(true);
   const button = clone.querySelector("button");
-  button.dataset.id = folder;
+  button.dataset.id = tag;
+  button.textContent = tag;
   button.addEventListener("click", () => {
     document
-      .querySelectorAll(`.images [data-id]="${folder}"`)
-      .classList.toggle("is-hidden");
+      .querySelectorAll(".image-template__wrapper")
+      .forEach((template) => {
+        if (!template.dataset[tag]) {
+          template.classList.add("is-hidden");
+        } else {
+          template.classList.remove("is-hidden");
+        }
+      });
     button.classList.toggle("is-active");
   });
+  document.querySelector(".tags").appendChild(button);
 };
 
 getMetadata();
