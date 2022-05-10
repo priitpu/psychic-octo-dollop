ab49e806cbb9ddee122d966acb18cfb7ca3949ef
Priit Puru <priit.puru@twn.ee>
2022-05-10 14:38:41 +0300

    .

--- js.js
+++ js.js
@@ -2,23 +2,33 @@ const getMetadata = async () => {
   const req = await fetch("./library/mtime.json");
   const res = await req.json();
   const folders = Object.keys(res);
-  const promises = folders.map((folder) => {
-    getFiles(folder);
+  const promises = folders.map(async (folder) => {
+    return await getFiles(folder);
   });
   const allPromises = await Promise.allSettled(promises);
-  console.log(allPromises);
+  const images = allPromises
+    .filter((thing) => thing.status === "fulfilled")
+    .map((thing) => thing.value);
+  images.forEach((image) => generateImageHTML(image));
 };
 
 const getFiles = async (folder) => {
-  const req = await fetch(`./library/${folder}/metadata.json`);
-  const metadata = await req.json();
-  return Promise.resolve(constructThing(folder, metadata));
+  try {
+    const req = await fetch(`./library/images/${folder}.info/metadata.json`);
+    const metadata = await req.json();
+    return { metadata, folder };
+  } catch (err) {}
 };
 
-const constructThing = (folder, metadata) => {
-  const src = `./library/${folder}/${metadata.name}.${metadata.ext}`;
-  const tags = [...metadata.tags];
-  return { src, tags };
+const generateImageHTML = (image) => {
+  const temp = document.querySelector(".image-template");
+  const clone = temp.content.cloneNode();
+  clone.querySelector("image-template__tags").innerHTML =
+    image.metadata.tags.join(", ");
+  clone.querySelector(
+    "image-template__img"
+  ).src = `./library/images/${image.folder}.info)/${image.metadata.name}.${image.metadata.ext}`;
+  document.body.appendChild(clone);
 };
 
 getMetadata();
