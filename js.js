de5bfd91d304b020f587658daf5b348dea495f80
Priit Puru <priit.puru@twn.ee>
2022-05-10 13:48:49 +0300

    .

--- js.js
+++ js.js
@@ -1,7 +1,23 @@
-var viiner = async () => {
+const getMetadata = async () => {
   const req = await fetch("./library/mtime.json");
-  const res = req.json();
-  console.log(res);
+  const res = await req.json();
+  const folders = Object.keys(res);
+  const promises = folders.map((folder) => {
+    getFiles(folder);
+  });
+  Promise.allSettled(promises).then((response) => console.log(response));
 };
 
-viiner();
+const getFiles = async (folder) => {
+  const req = await fetch(`./library/${folder}/metadata.json`);
+  const metadata = await req.json();
+  return constructThing(folder, metadata);
+};
+
+const constructThing = (folder, metadata) => {
+  const src = `./library/${folder}/${metadata.name}.${metadata.ext}`;
+  const tags = [...metadata.tags];
+  return { src, tags };
+};
+
+getMetadata();
